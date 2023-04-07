936cc7be9635e8b1380e8f84f697927f6cf184d0
Božidar Nađ <48714324+flaeryy@users.noreply.github.com>
2023-04-07 12:01:16 +0200

    adding app key for deploy

--- config/server.js
+++ config/server.js
@@ -1,10 +1,12 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "myAdminSecret"),
+    },
   },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  app: {
+    keys: ["myKeyA", "myKeyB"],
   },
 });
