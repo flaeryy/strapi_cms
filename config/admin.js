d1f6156ba8bdd75cf5e4956a1cbf602d818d0414
Božidar Nađ <48714324+flaeryy@users.noreply.github.com>
2023-04-07 12:57:33 +0200

    plugins for users permissions

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,12 @@
 module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
-  },
-  apiToken: {
-    salt: env('API_TOKEN_SALT'),
-  },
-  transfer: {
-    token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+  // ...
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "myAdminSecret"),
+      apiToken: {
+        // Set the apiToken.salt variable
+        salt: env("ADMIN_API_TOKEN_SALT", "mySalt"),
+      },
     },
   },
 });
